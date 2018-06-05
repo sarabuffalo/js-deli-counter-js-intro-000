6af0a21d8e8ebe140d9f91e4c9b7983aec243a11
sarabuffalo <sep.14094@yahoo.com>
2018-06-05 14:37:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function nowServing(katzDeli) {
 function currentLine(katzDeli) {
 const line = [];
 for (let i=0; i < katzDeli.length; i++) {
-  line.push(`${i+1}. ${katzDeli[i]}`)
+  line.push(` ` + [i+1].  + katzDeli[i])
 } if (katzDeli.length>1) {
-    return (`The line is currently:`  + line)
+    return (`The line is currently:` + line)
   } else {
   return "The line is currently empty."
   }
