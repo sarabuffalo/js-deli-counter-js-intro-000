0ef4699a7c4a8b529f6ea71d0fda3f6f83e45b4b
sarabuffalo <sep.14094@yahoo.com>
2018-06-05 14:22:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function nowServing(katzDeli) {
 }
 
 function currentLine(katzDeli) {
-var line = [];
-for (let i=0, l=katzDeli.length; i<l; i++) {
+const line = [];
+for (let i=0; i < katzDeli.length; i++) {
   line.push(`${i+1}. ${katzDeli[i]}`)
 } if (katzDeli.length>1) {
     return `The line is currently: ${line.shift()}. ${katzDeli}`
